447b51e7e4b07ebfa2acfd3fbdc1d5b4d24ede18
Fabien Winkler <fabien.winkler@outlook.fr>
2024-01-07 23:39:51 +0100

    chore: update content

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:fabien.winkler@outlook.fr">
-          fabien.winkler@outlook.fr
+        <a className="underline" href="mailto:fabienwinkler@outlook.fr">
+          fabienwinkler@outlook.fr
         </a>{" "}
         or through this form.
       </p>
--- lib/data.ts
+++ lib/data.ts
@@ -1,104 +1,108 @@
-import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import ethiclickImg from "@/public/ethiclick.png";
-import geomatikaImg from "@/public/geomatika.png";
+import React from 'react';
+import { CgWorkAlt } from 'react-icons/cg';
+import { FaReact } from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
+import corpcommentImg from '@/public/corpcomment.png';
+import ethiclickImg from '@/public/ethiclick.png';
+import geomatikaImg from '@/public/geomatika.png';
+import mecamobileImg from '@/public/mecamobile.png';
+import portfolioImg from '@/public/portfolio.png';
 
 export const links = [
   {
-    name: "Home",
-    hash: "#home",
+    name: 'Home',
+    hash: '#home'
   },
   {
-    name: "About",
-    hash: "#about",
+    name: 'About',
+    hash: '#about'
   },
   {
-    name: "Projects",
-    hash: "#projects",
+    name: 'Projects',
+    hash: '#projects'
   },
   {
-    name: "Skills",
-    hash: "#skills",
+    name: 'Skills',
+    hash: '#skills'
   },
   {
-    name: "Experience",
-    hash: "#experience",
+    name: 'Experience',
+    hash: '#experience'
   },
   {
-    name: "Contact",
-    hash: "#contact",
-  },
+    name: 'Contact',
+    hash: '#contact'
+  }
 ] as const;
 
 export const experiencesData = [
   {
-    title: "Web / Mobile developer",
-    location: "Bayonne, France",
-    description:
-      "I graduated after 8 months of studying (including 2 month stage). I immediately found a job as a front-end developer.",
+    title: 'Web / Mobile developer',
+    location: 'Bayonne, France',
+    description: 'I graduated after 8 months of studying (including 2 month stage). I immediately found a job as a front-end developer.',
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: '2019'
   },
   {
-    title: "Full-Stack Developer",
-    location: "Bayonne, France",
+    title: 'Full-Stack Developer',
+    location: 'Bayonne, France',
     description:
       "After 4 years as web / mobile developer, I'm now a full-stack developer working as a freelancer. My stack includes React, React Native, VueJS, Nuxt.js, Next.js, TypeScript, and PostgreSQL. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2023 - present",
-  },
+    date: '2023 - present'
+  }
 ] as const;
 
 export const projectsData = [
   {
-    title: "Drawer",
-    description:
-      "I worked to build a canvas free hand draw for an organization.",
-    tags: ["Vite", "Typescript", "Vitest", "CSS"],
-    imageUrl: corpcommentImg,
+    title: 'Mécamobile',
+    description: 'A web site including home made back office for an independant mecanician moving home with a equipped truck.',
+    tags: ['VueJS', 'Vuetify', 'TinyMCE', 'NodeJS', 'Express', 'Sequelize'],
+    imageUrl: mecamobileImg
   },
   {
-    title: "Ethiclick",
-    description:
-      "I create a fullstack web app (REST architecture).",
-    tags: ["React Native", "TypeScript", "React Native Paper", "AdonisJS", "Redux"],
-    imageUrl: ethiclickImg,
+    title: 'Drawer',
+    description: 'I worked to build a canvas free hand draw for an organization.',
+    tags: ['Vite', 'Typescript', 'Vitest', 'CSS'],
+    imageUrl: corpcommentImg
   },
   {
-    title: "Géomatika (Isigéo)",
-    description:
-      "A web / mobile app geographic information system for data analytics.",
-    tags: ["Javascript (jQuery)", "Bootstrap", "PostgreSQL", "Fullcalandar", "Cordova"],
-    imageUrl: geomatikaImg,
+    title: 'Géomatika (Isigéo)',
+    description: 'A web / mobile app geographic information system for data analytics.',
+    tags: ['Javascript (jQuery)', 'Bootstrap', 'PostgreSQL', 'Fullcalandar', 'Cordova'],
+    imageUrl: geomatikaImg
+  },
+
+  {
+    title: 'Ethiclick',
+    description: 'A mobile app listing all committed traders (ecologically) around you.',
+    tags: ['React Native', 'TypeScript', 'React Native Paper', 'AdonisJS', 'Redux'],
+    imageUrl: ethiclickImg
   },
   {
-    title: "Portfolio (yes, this website)",
-    description:
-      "My currently personnal portfolio.",
-    tags: ["React", "TypeScript", "Next.js", "Resend", "Tailwind"],
-    imageUrl: geomatikaImg,
-  },
+    title: 'Portfolio (this one)',
+    description: 'My currently personnal portfolio.',
+    tags: ['React', 'TypeScript', 'Next.js', 'Resend', 'Tailwind', ''],
+    imageUrl: portfolioImg
+  }
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "React Native",
-  "Next.js",
-  "Nuxt.js",
-  "Node.js",
-  "Git",
-  "Bootstrap",
-  "Tailwind",
-  "Figma",
-  "Redux",
-  "Express",
-  "PostgreSQL",
-  "AdonisJS",
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'React Native',
+  'Next.js',
+  'Nuxt.js',
+  'Node.js',
+  'Git',
+  'Bootstrap',
+  'Tailwind',
+  'Figma',
+  'Redux',
+  'Express',
+  'PostgreSQL',
+  'AdonisJS'
 ] as const;
